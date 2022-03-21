cae37f527250062c04b51046b9283332768fff90
Olger Siebinga <o.siebinga@tudelft.nl>
2022-03-21 11:40:30 +0100

    updated car following example plot to reflect new version of the paper

--- find_carfollowing_examples.py
+++ find_carfollowing_examples.py
@@ -24,10 +24,10 @@ from tactical_evaluation import load_encrypted_pickle
 data: HighDDataset
 
 
-def get_examples_of_carfollowing(axis_item, dataset_id=1, target_number_of_examples=3, path_to_data_folder=os.path.join('data', 'HighD', 'data')):
-    data = load_encrypted_pickle(os.path.join(path_to_data_folder, '%02d.pkl' % dataset_id))
+def get_examples_of_carfollowing(axis_item, dataset_id=12, colored_examples=3, grey_examples=52):
+    data = load_encrypted_pickle('data/%02d.pkl' % dataset_id)
     count = 0
-    colors = ['tab:green', 'tab:purple', 'tab:brown', 'tab:pink', 'tab:gray', 'tab:olive', 'tab:cyan', 'tab:red']
+    target_number_of_examples = colored_examples + grey_examples
 
     for vehicle_id in data.track_meta_data.numFrames.sort_values(ascending=False).index:
         preceding_ids = data.track_data.loc[data.track_data['id'] == vehicle_id, 'precedingId'].unique()
@@ -39,9 +39,17 @@ def get_examples_of_carfollowing(axis_item, dataset_id=1, target_number_of_examp
             inverse_ttc = 1 / ttc[ttc != 0]
             thw = demo_data['thw'].to_numpy()[ttc != 0]
 
-            axis_item.plot(thw, inverse_ttc, label='Dataset %02d, Vehicle %d ' % (dataset_id, vehicle_id), linewidth=0.7, color=colors[count])
-            axis_item.plot(thw[-1], inverse_ttc[-1], color='tab:orange', marker='o', fillstyle='none', markersize=3., linestyle='')
-            axis_item.plot(thw[0], inverse_ttc[0], color='k', marker='d', markersize=3., fillstyle='none', linestyle='')
+            color = 'tab:green' if count < colored_examples else 'lightgrey'
+            label ='Dataset %02d, Vehicle %d ' % (dataset_id, vehicle_id) if count < colored_examples else None
+            line_width = 1. if count < colored_examples else 0.7
+            z_order = 2 if count < colored_examples else 1
+
+            axis_item.plot(thw, inverse_ttc, label=label, linewidth=line_width, color=color, zorder=z_order)
+
+            if count < colored_examples:
+                axis_item.plot(thw[-1], inverse_ttc[-1], color='tab:orange', marker='o', fillstyle='none', markersize=3., linestyle='', zorder=3)
+                axis_item.plot(thw[0], inverse_ttc[0], color='k', marker='d', markersize=3., fillstyle='none', linestyle='', zorder=3)
+
             count += 1
             if count == target_number_of_examples:
                 break
